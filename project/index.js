0e1417ea9f73dad8b89be5371a090250f4f8f051
Bryan Miller <bmilcs@yahoo.com>
2023-05-13 16:03:31 -0400

    Refactored server using fs/promises

--- project/index.js
+++ project/index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const http = require("http");
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 const myLogger = require("./logger");
 
-const server = http.createServer((req, res) => {
-  myLogger.emit("log", `Request received on URL: ${req.url}`);
+const server = http.createServer(async (req, res) => {
+  myLogger.emit("log", `Request received for URL: "${req.url}"`);
 
   // create full path of the requested page
   const filePath = path.join(
@@ -14,42 +14,45 @@ const server = http.createServer((req, res) => {
     req.url === "/" ? "index.html" : `${req.url}.html`,
   );
 
-  // retrieve the contents of the requested file
-  fs.readFile(filePath, "utf-8", (pageError, pageData) => {
-    if (pageError) {
-      // something went wrong with the original read file
-      const isFileNotFound = pageError.code === "ENOENT";
-
-      if (isFileNotFound) {
-        myLogger.emit("error", `File not found: ${req.url}`);
-
+  // retrieve file & send response
+  try {
+    const pageData = await fs.readFile(filePath, "utf-8");
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(pageData);
+    myLogger.emit(
+      "log",
+      `Requested page successfully sent to client: "${filePath}"`,
+    );
+  } catch (pageError) {
+    const isFileNotFound = pageError.code === "ENOENT";
+    if (isFileNotFound) {
+      myLogger.emit("error", `File not found: "${filePath}"`);
+      try {
         const errorPagePath = path.join(__dirname, "html", "404.html");
-
-        fs.readFile(errorPagePath, "utf-8", (errorPageError, errorPageData) => {
-          if (errorPageError) {
-            myLogger.emit("error", "404 Page Not Found");
-            throw errorPageError;
-          }
-
-          // display error page:
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.end(errorPageData);
-        });
-      } else {
-        // server error: display it on page
+        const errorPageData = await fs.readFile(errorPagePath, "utf-8");
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(errorPageData);
+        myLogger.emit("log", "404 Page sent to client.");
+      } catch (errorPageError) {
         res.writeHead(500);
-        res.end(`Server error: ${pageError.code}`);
+        res.end(`Server error: "${errorPageError.code}"`);
+        myLogger.emit(
+          "error",
+          `Server error: "${errorPageError.code}" (404 File)`,
+        );
       }
     } else {
-      // read file succeeded:
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(pageData);
+      res.writeHead(500);
+      res.end(`Server error: ${pageError.code}`);
+      myLogger.emit("error", `Server-related error: "${pageError.code}"`);
     }
-  });
+  }
+
+  myLogger.emit("log", "Request handling complete.");
 });
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  myLogger.emit("log", `Server launched on port: ${HOST}:${PORT}`);
+  myLogger.emit("log", `Server launched on port: ${PORT}`);
 });
